327b41d198a09f80c5ec2a35051b547b6a4362da
mystical-calculator-5374 <bbrower@smu.edu>
2020-01-25 04:50:25 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,7 @@
 def roll_call_dwarves(names)# code an argument here
   # Your code here
   names.each_with_index do |name, i|
-    puts "#{i + 1}. #{name}"
+    p "#{i + 1}. #{name}\n"
   end
 end
 
