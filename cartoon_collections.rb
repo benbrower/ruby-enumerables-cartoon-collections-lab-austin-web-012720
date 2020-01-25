03d53557720bfda613214484b1c13b782c08f3f1
mystical-calculator-5374 <bbrower@smu.edu>
2020-01-25 05:05:09 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,8 +15,8 @@ def long_planeteer_calls(calls)# code an argument here
   calls.any? { |e| e.length > 4 }
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(cheese)# code an argument here
   # the array below is here to help
-  c
-  heese_types = ["cheddar", "gouda", "camembert"]
+  cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese.find { |e| cheese_types.include?(e) }
 end
