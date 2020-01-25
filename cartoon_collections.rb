891520d637e9def858d6d827103736f8e77aea59
mystical-calculator-5374 <bbrower@smu.edu>
2020-01-25 04:28:39 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,13 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(names)# code an argument here
   # Your code here
+  names.each_with_index do |name, i|
+    puts "#{i}. #{name}"
+  end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(calls)# code an argument here
   # Your code here
+  puts calls.map! { |e| e + "!" }
 end
 
 def long_planeteer_calls# code an argument here
